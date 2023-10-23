17068bd7d7a12e1a000766854b081ad55203133b
lzhangcs <63130594+lzhangcs@users.noreply.github.com>
2023-10-22 22:02:45 -0700

    update
    update

--- FizzBussConnectorMaven/src/main/java/com/facebook/presto/fizzbuzz/FizzBuzzClient.java
+++ FizzBussConnectorMaven/src/main/java/com/facebook/presto/fizzbuzz/FizzBuzzClient.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.example;
+package com.facebook.presto.fizzbuzz;
 
 import com.facebook.airlift.json.JsonCodec;
 import com.google.common.base.Function;
